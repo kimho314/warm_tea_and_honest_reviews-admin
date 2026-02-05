8555181b08be1a08cfb33452054dcd7dd795a049
Hoseop.kim <kimho314@gmail.com>
2026-02-05 17:32:54 +0900

    [feat]: add warm tea and honest reviews server project

---
+++ server/warm-tea-and-honest-reviews/src/main/java/com/luna/warmteaandhonestreviews/WarmTeaAndHonestReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.luna.warmteaandhonestreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmTeaAndHonestReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarmTeaAndHonestReviewsApplication.class, args);
+    }
+
+}
