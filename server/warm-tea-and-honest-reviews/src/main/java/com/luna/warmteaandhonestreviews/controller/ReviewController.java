9d329ab6a8320ebb3ccb4b5e0761401838c85705
Hoseop.kim <kimho314@gmail.com>
2026-02-07 20:15:10 +0900

    [feat]: change response time of get reviews api to ResponsEntity

--- server/warm-tea-and-honest-reviews/src/main/java/com/luna/warmteaandhonestreviews/controller/ReviewController.java
+++ server/warm-tea-and-honest-reviews/src/main/java/com/luna/warmteaandhonestreviews/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.luna.warmteaandhonestreviews.controller;
 import com.luna.warmteaandhonestreviews.dto.GetReviewsRespDto;
 import com.luna.warmteaandhonestreviews.service.ReviewService;
 import org.jspecify.annotations.NonNull;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +20,10 @@ public class ReviewController {
     }
 
     @GetMapping()
-    public GetReviewsRespDto getReviews(@NonNull @RequestParam(defaultValue = "0") Integer page,
+    public ResponseEntity<GetReviewsRespDto> getReviews(
+        @NonNull @RequestParam(defaultValue = "0") Integer page,
         @NonNull @RequestParam(defaultValue = "30") Integer offset) {
-        return reviewService.getReviews("162a59e1-571f-42a3-a41a-edc83b03618a", page, offset);
+        return ResponseEntity.ok(
+            reviewService.getReviews("162a59e1-571f-42a3-a41a-edc83b03618a", page, offset));
     }
 }
